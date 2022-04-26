988e3a78e17ec3105d9240274d638745974e53e6
jonInSweden <jon@vrconstructed.com>
2022-04-26 16:10:41 +0200

    First commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,32 @@
-const React = require('react');
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
-class App extends React.Component {
-  render() {
+function App() {
+  const [ip, setIP] = useState('');
+  const React = require('react');
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/')
+    console.log(res.data);
+    setIP(res.data.IPv4)
+}
+
+useEffect( () => {
+  //passing getData method to the lifecycle method
+  getData()
+
+}, [])
+
     return (
-      <h1>Header from React</h1>
+      <>
+      <div className='App'>
+          <h1>Welcome</h1>
+          <h2>Your IP Address is</h2>
+          <h4>{ ip }</h4>
+          <h1>The Date is {new Date().toLocaleString()}</h1>
+        </div>
+      </>
     )
   }
-}
+
 
 export default App;
